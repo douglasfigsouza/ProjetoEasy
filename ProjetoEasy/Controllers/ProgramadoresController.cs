69b064d5233cad4462383434d0a763a5a3dc7af7
Douglas da Mata <douglasfigsouza@gmail.com>
2017-05-09 10:05:48 -0300

    Criação do controller programadores

---
+++ ProjetoEasy/Controllers/ProgramadoresController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoEasy.Controllers
+{
+    public class ProgramadoresController : Controller
+    {
+        // GET: Programadores
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
